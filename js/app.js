1a13812efc20bcb958d333ec90c5ccabc1b84da2
Abdelouahab Djoudi <djoudi@AppsBunches.com>
2023-05-27 09:44:57 +0100

    update effect

---
+++ js/app.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+
+document.querySelector('.box')
+  // $(".box").hide(5000)
+	//$(".box").show(5000)
+	//$(".box").toggle(5000)
+	// $(".box").fadeOut(5000)
+    // $(".box").fadeIn(5000)
+    
+     // $(".box").fadeToggle(5000)
+    // $(".box").slideUp(5000)
+   //  $(".box").slideDown(5000)
+   //  
+   // $(".box").slideToggle(5000)
+   // 
+  // $(".box").toggle(5000).delay(2000).slideToggle(5000)
+
+	
+})
